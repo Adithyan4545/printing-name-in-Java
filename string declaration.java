7b165095aab6b513a5a92202af903a579fc25394
Adithyan4545 <adithyan.r2024cce@sece.ac.in>
2025-07-09 09:26:07 +0530

    Create string declaration.java

---
+++ string declaration.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String name,street,state,country;
+	    name="Adithyan";
+	    street="Jeeva Nagar";
+	    state="TN";
+	    country="India";
+		System.out.println("name="+name);
+		System.out.println("street="+street);
+		System.out.println("state="+state);
+		System.out.println("country="+country);
+	}
+}
