8aca52971d1b48c873d51e69ab8c8298a1f0e71e
Adithyan4545 <adithyan.r2024cce@sece.ac.in>
2025-07-09 09:15:27 +0530

    Create basic program.java

---
+++ basic program.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int tomato=2;
+	    int_weight machine=0;
+	    weight_machine=tomato;;
+	    
+		System.out.print(weight_machine);
+	
+	}
+}
