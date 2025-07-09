24c9be83ce15ab2260bc1600580836eee3c202b3
Adithyan4545 <adithyan.r2024cce@sece.ac.in>
2025-07-09 11:24:17 +0530

    Create boolean.java

---
+++ boolean.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//java program to print the age
+/*Author is Arun
+*Date=09.07.2025
+*/
+public class Main
+{
+	public static void main(String[] args)
+	{
+	   //TODO Auto generated method Stud
+	   boolean gate=false;
+	   System.out.println("Boolean gate="+gate);
+	   
+	}
+}
